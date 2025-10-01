2e44588186f8456bcb3fbdcf330559eafc7885fa
coderelham <mdelham.official@gmail.com>
2025-10-02 01:33:01 +0600

    added counter section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
+import States from './Components/States/States';
+import OrderContainer from './Components/OrderContainer/OrderContainer';
+
+// Load Data
+const loadOrders = () => fetch('/orders.json').then(res => res.json())
 
 const App = () => {
+
+    const orderPromise = loadOrders()
+
     return (
         <div>
             <header className='w-11/12 mx-auto py-4'>
@@ -10,6 +18,11 @@ const App = () => {
             </header>
             <section>
                 <Header>Kitchen Room</Header>
+            </section>
+            <section>
+                <Suspense fallback={<span>Loading...⌛</span>}>
+                    <OrderContainer orderPromise={orderPromise}></OrderContainer>
+                </Suspense>
             </section>
         </div>
     );
---
+++ src/Components/OrderContainer/OrderContainer.jsx
@@ -0,0 +1,14 @@
+import React, { use } from 'react';
+import States from '../States/States';
+
+const OrderContainer = ({ orderPromise }) => {
+    const orders = use(orderPromise);
+    console.log(orders);
+    return (
+        <div>
+            <States></States>
+        </div>
+    );
+};
+
+export default OrderContainer;
