e3846192e8d99d98545e2abcfb3c192c7dd1de45
coderelham <mdelham.official@gmail.com>
2025-10-01 16:21:15 +0600

    added all resources

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <div className="text-4xl text-red-500">
+                Cooking.... 🧑‍🍳
+            </div>
+        </div>
+    );
+};
+
+export default App;
